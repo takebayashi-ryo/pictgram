8ff87c46a989b2f54ccecfcdf9de642b8c86178a
ryotakebayashi <rrr.7164.rrr@gmail.com>
2018-09-15 15:24:20 +0900

    pict commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }
 
   has_secure_password
 
   has_many :topics
   has_many :favorites
   has_many :favorite_topics, through: :favorites, source: 'topic'
-  
+
 end
